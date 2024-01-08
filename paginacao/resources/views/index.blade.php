655a098d555d44d2fffe344cfcb5ab34dda84db8
Marwam Malta <marwamprog.js@gmail.com>
2024-01-08 19:51:43 -0300

    feat: Criando paginacao

---
+++ paginacao/resources/views/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}" >
+    <title>Páginação</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    
+    <div class="container mt-5">
+        <div class="card text-center">
+            
+            <div class="card-header">
+                Tabela de Clientes
+            </div>
+            
+            <div class="card-body">
+
+                <h5 class="card-title">Exibindo 10 clientes de 1000 (1 a 10)</h5>
+
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Sobrenome</th>
+                            <th scope="col">E-mail</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($clientes as $cliente)
+                            <tr>
+                                <td>{{ $cliente->id }}</td>
+                                <td>{{ $cliente->nome }}</td>
+                                <td>{{ $cliente->sobrenome }}</td>
+                                <td>{{ $cliente->email }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            
+            <div class="card-body">
+                {{ $clientes->links() }}
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
