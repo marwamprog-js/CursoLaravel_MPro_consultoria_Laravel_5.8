c849a536d7c77e8202b0f1ce4cd12dad7b35e057
Marwam Malta <marwamprog.js@gmail.com>
2024-01-08 10:35:26 -0300

    feat: primeiro middleware

--- middlewares/app/Http/Middleware/PrimeiroMiddleware.php
+++ middlewares/app/Http/Middleware/PrimeiroMiddleware.php
@@ -3,24 +3,23 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
-class RedirectIfAuthenticated
+class PrimeiroMiddleware
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
-        }
 
+        Log::debug("Passou pelo primeiro middleware");
+
+        // return response('test');
         return $next($request);
     }
 }
--- middlewares/routes/web.php
+++ middlewares/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\PrimeiroMiddleware;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Route;
+
+Route::get('/usuarios', 'UsuarioControlador@index')
+    ->middleware(PrimeiroMiddleware::class);
