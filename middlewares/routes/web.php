22d85c375369bc133c8387e0b2eceec634144a0a
Marwam Malta <marwamprog.js@gmail.com>
2024-01-08 11:04:10 -0300

    feat: Adicionando o middleware para todas as rotas

--- middlewares/routes/web.php
+++ middlewares/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\PrimeiroMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +15,6 @@ use App\Http\Middleware\PrimeiroMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/usuarios', 'UsuarioControlador@index');
+Route::get('/teste', function() {
+    return "Test";
+});
