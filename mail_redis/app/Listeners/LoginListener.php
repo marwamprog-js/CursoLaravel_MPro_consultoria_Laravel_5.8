7955e659a78136596edcc43f0c36b5a27dd53e6f
Marwam Malta <marwamprog.js@gmail.com>
2024-01-10 08:57:30 -0300

    Criando Eventos

---
+++ mail_redis/app/Listeners/LoginListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class LoginListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Login $event)
+    {
+        info($event->user->name);
+        info($event->user->email);
+    }
+}
